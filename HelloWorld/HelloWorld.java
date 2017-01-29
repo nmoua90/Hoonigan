8e52f300a15a9a4c8dc95cffcea464a83b3f30d9
Nhia Moua <n.moua9@gmail.com>
2017-01-29 02:24:59 -0600

    Updating file again.

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,6 +1,8 @@
 /**
  * Created by Izhar on 1/28/2017.
+ * Let's print our names in the main method, to show that we can update code on GITHUB.
  */
+ 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Izhar");
