380a8d6d61c4b3666c29b6dfeb7a88e1fc558ae7
Nhia Moua <n.moua9@gmail.com>
2017-02-09 04:59:42 -0600

    Touched up tons of code. Added comments to all classes. Generated JavaDocs for all classes.

---
+++ Assignment 1 Final Code/This Folder Contains All Files Found In The Zip File/LibraryUI.java
@@ -0,0 +1,20 @@
+package hoonigan;
+
+/**LibraryUI class
+ * 
+ * This class is the starting point for our software. Within the main method, the entire program is initialized and run.
+ * @author Hoonigan
+ * 
+ */
+
+public class LibraryUI{
+	/**main(String[] args)
+	 * This main method initializes the Library program, and runs it.	
+	 * @param args - a String of arguments
+	 */
+	public static void main(String[] args){
+		InitializeProgram startProgram = new InitializeProgram();
+		startProgram.runProgram();
+	}
+	
+}
